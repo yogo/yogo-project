fddf5a67c445983f6bd9828d8f0ce2498536b49c
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-22 15:33:36 -0600

    Adding filename mangaling for file assets

--- lib/yogo/collection/asset/model_properties.rb
+++ lib/yogo/collection/asset/model_properties.rb
@@ -13,19 +13,32 @@ module Yogo
             def store_dir
               File.join('#{Configuration.collection.asset.storage_dir}', '#{model.collection.collection_storage_name}')
             end
+            
+            def filename
+              # Digest::MD5.hexdigest(self.read)
+              UUIDTools::UUID.timestamp_create
+            end
           }, __FILE__, __LINE__+1
           
           model.class_eval do
             without_auto_validations do
-              property :content_type, String
-              property :description,  String
-              property :asset_file,   String
+              property :content_type,       String
+              property :description,        String
+              property :asset_file,         String
+              property :original_filename,  String
             end
             
-            validates_uniqueness_of :asset_file
+            # validates_uniqueness_of :asset_file
             
             mount_uploader :file, uploader, :mount_on => :asset_file
             after :file=, :write_file_identifier
+            after :file=, :set_original_filename
+            
+            private
+            
+            def set_original_filename
+              attribute_set(:original_filename, file.send(:original_filename))
+            end
           end
         end
       end # ModelProperties
