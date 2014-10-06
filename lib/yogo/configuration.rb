8620845f619607cb00666d2476d6e23005bf5eb2
Sean Cleveland <sean.b.cleveland@gmail.com>
2014-10-06 12:49:32 -0600

    fix syntaxt

--- lib/yogo/configuration.rb
+++ lib/yogo/configuration.rb
@@ -1,7 +1,7 @@
 require 'configatron'
 
 module Yogo
-  Configuration = configuration #Configatron::Store.new
+  Configuration = configatron #Configatron::Store.new
   Configuration.random_uuid = Configatron::Dynamic.new { UUIDTools::UUID.timestamp_create }
   Configuration.collection.set_default(:data_repository_name, :collection_data)
   Configuration.collection.asset.set_default(:storage_dir, 'asset_collection')
